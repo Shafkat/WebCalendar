c59c38fda19301b7dadc54b83effc09a5d547055
Shafkat <shafkatulhaque@live.ca>
2012-03-13 22:37:51 -0400

    Added all files. Ready to Code.

---
+++ javascript/script.js
@@ -0,0 +1,24 @@
+head.js("https://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js","./javascript/ui.js","./javascript/touch.js", function (){
+$('.touchBox').draggable({revert:true});
+	$(".dropArea").droppable({
+    drop: function( event, ui ) {
+        //$(ui.draggable).remove();
+        $(this).css({'border':'#777 solid 3px','background':'#eee'});
+    },
+    over: function(event, ui) {
+        $(this).css({'border':'#a33 solid 3px','background':'#faa'});
+    },
+    out: function (event, ui){
+        $(this).css({'border':'#777 solid 3px','background':'#eee'});
+    }
+	});
+});
+
+function loadKeys()
+{
+	
+	head.js("https://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js","./javascript/ui.js","./javascript/touch.js", function (){
+	$('.touchBox').draggable({revert:true});
+	});
+	
+}
